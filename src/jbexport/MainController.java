6ceaa2e4488caeeca9417cc7197874a7491f97bf
Artur Czocher <artczo@wasper.pl>
2015-12-20 22:56:14 +0100

    Common panel

--- src/jbexport/MainController.java
+++ src/jbexport/MainController.java
@@ -1,4 +1,33 @@
 package jbexport;
 
-public class MainController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+    @FXML
+    private TextField brand;
+
+    @FXML
+    private TextField title;
+
+    @FXML
+    private TextField size;
+
+    @FXML
+    private TextField id;
+
+    @FXML
+    private TextArea components;
+
+    @FXML
+    private TextArea liningComponents;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
 }
