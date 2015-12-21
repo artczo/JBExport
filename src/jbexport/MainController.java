10c8bbe74c9c3bab48926a1e4e668053e87838a4
Artur Czocher <artczo@wasper.pl>
2015-12-21 12:11:12 +0100

    Common panel

--- src/jbexport/MainController.java
+++ src/jbexport/MainController.java
@@ -1,9 +1,14 @@
 package jbexport;
 
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,6 +19,10 @@ public class MainController implements Initializable {
 
     @FXML
     private TextField title;
+
+    private int titleSize = 0;
+
+    final int MAX_SIZE = 60;
 
     @FXML
     private TextField size;
@@ -27,7 +36,23 @@ public class MainController implements Initializable {
     @FXML
     private TextArea liningComponents;
 
+    @FXML
+    private TextArea description;
+
+    @FXML
+    private ChoiceBox condition;
+
+    @FXML
+    private Label titleSizeLabel;
+
+    @FXML
+    private void keyReleasedTitleAction(KeyEvent event) {
+        event.consume();
+        titleSizeLabel.setText(String.format("%d znaków", (MAX_SIZE - title.getText().length())));
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
     }
+
 }
